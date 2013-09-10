746a1aaf5b53464e78df012905d08d884991708b
Fattaf <andrey.nashkolniy@bookrenter.com>
2013-09-10 22:19:19 +0300

    config simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,9 @@ require 'factory_girl_rails'
 require 'ffaker'
 require 'database_cleaner'
 require 'paperclip/matchers'
+require 'simplecov'
+
+SimpleCov.start
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
