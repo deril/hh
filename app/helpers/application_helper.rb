47c48bd245663b4002209a52cd2cd2bd4850a093
Dima Bignyak <bignyak@bigmir.net>
2013-12-26 00:24:01 +0200

    fix alert

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   # TODO: tests
 
   def alert_notifier
-    response = content_tag(:div, nil, class: "clear")
+    response = content_tag(:div, nil, class: "clear-legacy")
     response += content_tag(:div, flash[:notice], class: "notice") if flash[:notice].present?
     response += content_tag(:div, flash[:alert], class: "alert") if flash[:alert].present?
     response
