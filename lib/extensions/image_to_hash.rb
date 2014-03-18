a77410ae23134497f7206982b6bd6e9198b5f101
Andrey_Nashkolniy <andrey.nashkolniy@bookrenter.com>
2014-03-18 22:17:19 +0200

    #64115094 add lib fix + tests

--- lib/extensions/image_to_hash.rb
+++ lib/extensions/image_to_hash.rb
@@ -1,35 +1,38 @@
-# require 'RMagick'
+require 'RMagick'
 
 module ImageToHash
-  def make_hash(image_path)
-    image = QuickMagick::Image.read(image_path).first
-    return unless image
+  class HashMaker
+    class << self
+      def make_hash(image_path)
+        return unless File.exist?(image_path)
 
-    # FIXME: problem how to make image grayscale!!!
-    image = image.resize(65, 65).quantize(256, ImageMagick::GRAYColorspace).crop(1,1, 64, 64)
+        begin
+          image = Magick::Image.read(image_path).first
+        rescue
+          return nil
+        end
 
-    brightnesses = []
-    image.each_pixel{|pixel, r, c| brightnesses << (0.2126*pixel.red) + (0.7152*pixel.green) + (0.0722*pixel.blue) }
+        image = image.resize(65, 65).quantize(256, Magick::GRAYColorspace).crop(1,1, 64, 64)
 
-    average_brightness = brightnesses.inject(:+) / brightnesses.size
-    hash_brightness = ''
-    brightnesses.each{|elem| hash_brightness << ((elem < average_brightness) ? '0' : '1') }
-    hash_brightness.to_i(2).to_s(16)
-  end
+        brightnesses = []
+        image.each_pixel{|pixel, r, c| brightnesses << (0.2126*pixel.red) + (0.7152*pixel.green) + (0.0722*pixel.blue) }
 
-  def compare_hashes(hash_1, hash_2)
-    diff_count = 0
-    hash_1.split('').each_with_index{|el, i| diff_count += 1 unless el == hash_2[i] }
+        average_brightness = brightnesses.inject(:+) / brightnesses.size
 
-    diff_percentage = diff_count/(hash_1.size/100)
-    diff_percentage
-  end
+        hash_brightness = ''
+        brightnesses.each{|elem| hash_brightness << ((elem < average_brightness) ? '0' : '1') }
 
-  private
-    def statistics(all, diff)
-      p "diff = #{diff}"
-      p "all = #{all}"
-      p "percent = #{diff/(all/100)}%"
-      nil
+        hash_brightness.to_i(2).to_s(16)
+      end
+
+      def compare_hashes(hash_1, hash_2)
+        return unless hash_1.present? && hash_2.present?
+        diff_count = 0
+        hash_1.split('').each_with_index{|el, i| diff_count += 1 unless el == hash_2[i] }
+
+        diff_percentage = diff_count/(hash_1.size/100)
+        diff_percentage
+      end
     end
+  end
 end
