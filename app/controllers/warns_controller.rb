da3cc574002552cfcff2b6023f2d319c2a21fc34
Dima Bignyak <dima.bignyak@bookrenter.com>
2014-03-20 14:36:33 +0200

    make warns work

---
+++ app/controllers/warns_controller.rb
@@ -0,0 +1,10 @@
+class WarnsController < ApplicationController
+  before_action :add_warns, only: [:show]
+
+  def show
+    @warn = Warn.find_by(id: params[:id])
+    redirect_to root_url, flash: { alert: 'Warn not found' } and return unless @warn
+    @imgs = @warn.images.page(current_page)
+    @tags = get_uniq_tags_from(@imgs) if @imgs.present?
+  end
+end
