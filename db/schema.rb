bbd8a6236aa5693e130d2572d4c9cc19633c371a
Dima Bignyak <dima.bignyak@bookrenter.com>
2014-04-06 08:42:58 +0300

    add alt field to images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140318203135) do
+ActiveRecord::Schema.define(version: 20140406052444) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20140318203135) do
     t.datetime "image_updated_at"
     t.integer  "warn_id"
     t.string   "image_hash",         limit: 1024
+    t.string   "alt"
   end
 
   create_table "images_tags", force: true do |t|
