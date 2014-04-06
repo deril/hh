52f319b045eb260321e3e60fa5109a96c01adf45
Dima Bignyak <dima.bignyak@bookrenter.com>
2014-04-06 08:57:22 +0300

    add migration for making alts for all images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140406052444) do
+ActiveRecord::Schema.define(version: 20140406055506) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
