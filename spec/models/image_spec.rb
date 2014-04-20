ba23fecbf6ac76611887a636f9f124c540016f10
Dima Bignyak <bignyak@bigmir.net>
2014-04-20 14:37:47 +0300

    Revert "fix alts as google recomend"
    This reverts commit 9bdca775d11105822f766784edbb9659423148be.

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -94,7 +94,7 @@ describe Image do
       image = FactoryGirl.build(:image_with_tags)
       expect(image.alt).to be_nil
       image.save!
-      expect(image.alt).to eq(image.tags.sample.name)
+      expect(image.alt).to eq(image.tags.map(&:name).join(', '))
     end
   end
 
