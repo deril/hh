4069a4abe4a1a4e4870b5f1ccae915aa827519b9
fattaf <andrew.nashkolniy@gmail.com>
2014-11-30 22:40:10 +0200

    small update warns clr

--- spec/controllers/warns_controller_spec.rb
+++ spec/controllers/warns_controller_spec.rb
@@ -2,25 +2,30 @@ require 'rails_helper'
 
 describe WarnsController, :type => :controller do
 
-  let(:tag) { FactoryGirl.create(:tag) }
-  let(:image) { FactoryGirl.create(:image, tags: [tag]) }
-  let(:warn) { FactoryGirl.create(:warn, images: [image]) }
+  describe "#show" do
+    let(:tag) { FactoryGirl.create(:tag) }
+    let(:image) { FactoryGirl.create(:image, tags: [tag]) }
+    let(:warn) { FactoryGirl.create(:warn, images: [image]) }
 
-  describe "GET 'show'" do
-    it "responses redirect if not found" do
-      get :show, { id: 0 }
-      response.should redirect_to(root_path)
-      flash[:alert].should == 'Warn not found'
+    context 'if not found' do
+      it "redirects on root" do
+        get :show, { id: 0 }
+        expect(response).to be_redirect
+      end
+      it 'redirects with alert' do
+        get :show, { id: 0 }
+        expect(flash[:alert]).to eq('Warn not found')
+      end
     end
     it "responds success" do
       get :show, { id: warn.id }
-      response.should be_success
-    end 
+      expect(response).to be_success
+    end
     it "has warn, images and tags variables" do
       get :show, { id: warn.id }
-      assigns(:warn).should == warn
-      assigns(:imgs).should == [image]
-      assigns(:tags).should == [tag]
+      expect(assigns(:cur_warn)).to eq(warn)
+      expect(assigns(:imgs)).to eq([image])
+      expect(assigns(:tags)).to eq([tag])
     end
   end
 
