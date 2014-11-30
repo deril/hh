9d584ef6358a44c87178b6706137e710188a9330
fattaf <andrew.nashkolniy@gmail.com>
2014-11-30 15:05:28 +0200

    fix warn header

--- app/controllers/warns_controller.rb
+++ app/controllers/warns_controller.rb
@@ -4,6 +4,7 @@ class WarnsController < ApplicationController
   def show
     @warn = Warn.find_by(id: params[:id])
     redirect_to root_url, flash: { alert: 'Warn not found' } and return unless @warn
+
     @imgs = @warn.images.page(current_page)
     @tags = get_uniq_tags_from(@imgs) if @imgs.present?
   end
